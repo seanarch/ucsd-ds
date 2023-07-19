9d154e4d8f6a70f7c5e8df0bdbcbe6865492f7ae
seanarch <zhuqingyun@outlook.com>
2023-07-19 16:46:52 -0600

    Update week3

--- algorithmic toolbox/week2/5_fibonacci_number_again/fibonacci_huge.cpp
+++ algorithmic toolbox/week2/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -16,8 +16,34 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    if (n <= 1) {
+        return n;
+    }
+
+    long long previous = 0;
+    long long current = 1;
+
+    for (long long i = 2; i <= n; i++) {
+        long long temp = previous;
+        previous = current;
+        current = (temp + current) % m;
+
+        // Check for the Pisano period
+        if (previous == 0 && current == 1) {
+            long long pisano_period = i - 1;
+            long long remainder = n % pisano_period;
+            return get_fibonacci_huge_fast(remainder, m);
+        }
+    }
+
+    return current;
+}
+
+
 int main() {
     long long n, m;
     std::cin >> n >> m;
     std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
