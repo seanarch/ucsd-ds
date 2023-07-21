9e8529a90dcbe14062f556b34f948a711a46e687
seanarch <zhuqingyun@outlook.com>
2023-07-21 16:16:17 -0600

    Update week 3

--- algorithmic toolbox/week3/1_money_change/change.cpp
+++ algorithmic toolbox/week3/1_money_change/change.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+using namespace std;
+ 
 
-int get_change(int m) {
+
+int get_change(int n) {
   //write your code here
-  return n;
+  int numbersOfCoin = 0; 
+  while(n > 0) {
+    if (n >= 10) {
+      n = n - 10; 
+      numbersOfCoin++;
+    } else if ( n >= 5) {
+      n = n - 5; 
+      numbersOfCoin++; 
+    } else {
+      numbersOfCoin+=n;
+      break;
+    }
+  }
+
+  return numbersOfCoin;
+  return 0;
 }
 
 int main() {
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  cout << get_change(m) << '\n';
+
+  return 0;
 }
--- algorithmic toolbox/week3/3_car_fueling/car_fueling.cpp
+++ algorithmic toolbox/week3/3_car_fueling/car_fueling.cpp
@@ -8,7 +8,33 @@ using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
-    return -1;
+    int totalStops = 0;
+    int currentRefill = 0;
+    int lastRefill;
+
+    // Add the initial position (0) and the destination as stops
+    stops.insert(stops.begin(), 0);
+    stops.push_back(dist);
+
+    int n = stops.size();
+
+    while (currentRefill < n - 1) {
+        lastRefill = currentRefill;
+
+        // Find the farthest reachable stop from the current refill
+        while (currentRefill < n - 1 && stops[currentRefill + 1] - stops[lastRefill] <= tank)
+            currentRefill++;
+
+        // If the last refill is the same as the current one, it means we cannot reach the destination
+        if (currentRefill == lastRefill)
+            return -1;
+
+        // If we can reach the next refill station from the current one, increment the total stops count
+        if (currentRefill < n - 1)
+            totalStops++;
+    }
+
+    return totalStops;
 }
 
 
